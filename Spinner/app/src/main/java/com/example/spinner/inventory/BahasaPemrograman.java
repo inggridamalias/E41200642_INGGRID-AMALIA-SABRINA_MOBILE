c23a8aa5c95bf6c5c2bd75a5c6b7351c07a0ddcc
inggridamalias <inggridamalias99@gmail.com>
2021-10-07 20:21:28 +0700

    Create BahasaPemrograman.java

---
+++ Spinner/app/src/main/java/com/example/spinner/inventory/BahasaPemrograman.java
@@ -0,0 +1,25 @@
+package com.example.spinner.inventory;
+
+public class BahasaPemrograman {
+
+    //menampung bahasa pemrograman dan gambar logo
+    private String bahasapemrograman;
+    private int image;
+
+    public BahasaPemrograman() {
+    }
+
+    public String getBahasapemrograman() { return bahasapemrograman;
+    }
+    public void setBahasapemrograman(String bahasapemrograman) {
+        this.bahasapemrograman = bahasapemrograman;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
